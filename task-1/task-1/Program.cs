15194d512a700709a01c8a0cc115f8a12c80a1a7
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-04-10 09:37:16 +0500

    sum of entered numbers

---
+++ task-1/task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumSum();
+        }
+        public static void NumSum()
+        {
+            Console.Write("Enter a number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine($"Sum of entered numbers : {sum}.");
+        }
+    }
+}
