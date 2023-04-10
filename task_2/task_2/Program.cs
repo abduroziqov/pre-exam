8a3ff90fe2c8190d47025afbefba866a9f2ca9a0
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-04-10 10:48:32 +0500

    make a shape

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseNum();
+        }
+        public static void ReverseNum()
+        {
+            Console.Write("Enter a number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write(" *");
+                    }
+                    else
+                    {
+                        Console.Write("");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
